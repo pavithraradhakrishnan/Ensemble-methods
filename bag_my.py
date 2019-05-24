cf3471967bf5eab73469a2bc891f3261eb3b9321
Pavithra Radhakrishnan <pavithraradhakrishnan2@gmail.com>
2019-05-24 16:59:32 +0530

    Add files via upload

---
+++ bag_my.py
@@ -0,0 +1,441 @@
+import csv
+import math
+from random import seed
+from random import randrange
+from csv import reader
+from operator import itemgetter
+from random import randrange, seed
+import numpy as np
+from random import randrange, seed
+import matplotlib.pyplot as plt
+
+acc= []
+test_data = []
+x_test = []
+y_test = []
+acc1 = []
+
+with open("test1.csv", encoding='utf-8-sig') as csvfile:
+    rows = csv.reader(csvfile, delimiter=',')
+    data = [dota for dota in rows]
+    for x in range(len(data)):
+        ins = []
+        ins_y = []
+        for y in range(4):
+            ins.append(float(data[x][y]))
+
+        c = data[x][4]
+        x_test.append(ins)
+        y_test.append(int(c))
+
+def kfold(data, n):
+    #print(len(data))
+    #print(data)
+    np.random.shuffle(data)
+    #print(data)
+    #k = len(data) / n
+    k = 100
+    #print(k)
+    #print("data is ", data)
+
+    samples = list()
+    while (len(samples) < n):
+        np.random.shuffle(data)
+        #print("data", data)
+        sample = list()
+        #while len(sample) < k:
+        for index in range(int(k)):
+            index = randrange(len(data))
+            #print("index is",index)
+            #print("data index",data[index])
+            sample.append(data[index])
+        #print("sample is ", sample)
+        #print(len(sample))
+        samples.append(sample)
+    #print("samples together ", samples)
+    #print("length of samples", len(samples))
+    return samples
+def data_clean1(data):
+    x_train = []
+    y_train = []
+    test_data = []
+    for x in range(len(data)):
+        ins = []
+        ins_y = []
+        for y in range(4):
+            ins.append(float(data[x][y]))
+
+        c = data[x][4]
+        y_train.append(float(c))
+
+        x_train.append(ins)
+
+
+    #print(np.array(x_train))
+
+    x = np.array(x_train)
+    y = np.array(y_train)
+    #print("x in data clean,", x)
+    #print("y in data clean", y)
+        # x_train =
+        # y_train = y
+
+    with open("test1.csv", encoding='utf-8-sig') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        dota = [dota for dota in rows]
+
+    for k in range(len(dota)):
+        ins = []
+        for l in range(4):
+            ins.append(float(dota[k][l]))
+
+        test_data.append(ins)
+    test = np.array(test_data)
+        # print(test_data)
+
+    return x, y,test
+
+
+def normalise_data(x):
+    " we can normalise by subtracting the maximum value by the minimum value and  then divide by the standard deviation of x or divide by the range"
+
+    max_x = np.max(x,axis = 0)
+    min_x = np.min(x,axis = 0)
+    normalised_x = 1 - (max_x - x)/(max_x - min_x)
+    #print(normalised_x)
+    return normalised_x
+def calculate_sigmoid(x,weight):
+    #print("calculate sigmoid x",x)
+    #print("calculate sigmoid weight",weight)
+    h = 1.0 / (1 + np.exp(-np.dot(x, weight.T)))
+    return h
+
+def dot_product(a,b):
+    return sum(x*y for x,y in zip(a,b))
+
+def calculate_mean(m):
+    return (sum(x for x in m))/len(m)
+
+
+def calculate_gradient(weight, x, y):
+    # X = X - y.reshape(X.shape[0] -1)
+    # print("y in gradient descent",y)
+    # print("weight in gradient descent",weight)
+    # print("x in gradient descent",x)
+    h = calculate_sigmoid(x, weight)
+    # print("h in gradient decent",h)
+    first_calc = h - y.reshape(x.shape[0], -1)
+    final_calc = np.dot(first_calc.T, x)
+    # print("grad in gradient",final_calc)
+    return final_calc
+
+
+def calculate_cost(x, weight, y):
+    h = calculate_sigmoid(x, weight)
+    # print("y before squeeze",y)
+    y = np.squeeze(y)
+
+    h = np.mean((-(y * np.log(h)) - ((1 - y)) * np.log(1 - h)))
+
+    return h
+
+
+def grad_descent(x, y, weight, learning, gradient):
+    cost = calculate_cost(x, weight, y)
+    #print(cost)
+    iteration, change = 1, 1
+    while (change > gradient):
+        # iteration += 1
+        prev_cost = cost
+        # finding the weights and updating it after every itertation. The loop goes on unti the condition is met
+        weight = weight - (learning * calculate_gradient(weight, x, y))
+        cost = calculate_cost(x, weight, y)
+        # print("cost in grad",cost)
+
+        change = prev_cost - cost
+        # print("change in grad", change)
+        iteration = iteration + 1
+        # print("iteration no is",iteration)
+
+        # h = calculate_sigmoid(x, weight)
+
+        '''
+        difference = h - y
+        print("y in grad",y)
+        gradient = calculate_gradient(difference, h, y)
+        weight = minimise_loss(weight, learning, gradient)
+
+        current_cost = calculate_cost(h, y)
+        change = prev_cost - current_cost
+        '''
+    return weight, iteration
+
+def predict_val(weight,x):
+    prob = calculate_sigmoid(x,weight)
+    prediction = np.where(prob >= .5, 1, 0)
+    return np.squeeze(prediction)
+def logistic_regression(data):
+
+
+    X,y,test = data_clean1(data)
+    X = normalise_data(X)
+    one_column = np.ones((len(X), 1))
+    #print("The normalised input data is", X)
+    X = np.concatenate((one_column, X), axis=1)
+    weight = np.matrix(np.zeros(5))
+    # print("xin main", X)
+    # print("y in main", y)
+    # print("beta in main", weight)
+    weight, n = grad_descent(X, y, weight, 0.01, 0.01)
+    predict = predict_val(weight, X)
+    #print(len(predict))
+    #print("predict",predict)
+    #print("the computed theta value is", weight)
+    # print("difference between y and predicted y")
+    #print("The acutal output for the given input data is where '1' represents male and '0' representd female", y)
+    #print("The predicted output for the given data is", predict)
+    #print("len of y",len(y))
+
+    y_count = np.sum(y == predict)
+
+    Accuracy = (y_count/len(y))*100
+    #print("The calculated regression coefficients are", weight)
+    #print("total no of iterations taken", n)
+    #print("The count of the labels predicted", y_count)
+    #print("Accuracy is",(y_count/len(y))*100)
+    # print("y out",y)
+    # fit(x,y,1)
+    #acc.append(Accuracy)
+    #plot_reg(X, y, weight)
+    return X,y,weight,n
+
+def bagging_algo():
+    with open("train1.csv",encoding='utf-8-sig') as csvfile:
+        rows = csv.reader(csvfile,delimiter=',')
+        data = [data for data in rows]
+        #print(len(data))
+        data_splits = kfold(data, 5)
+        model = []
+        i = []
+        p = 0
+        for div_data in data_splits:
+            #print("count is",p)
+            p += 1
+            X,y,weight,n = logistic_regression(div_data)
+            model.append([X,y,weight,n])
+        #print("MODEL LENGTH IS",len(model))
+    return model
+
+
+        #print(acc)
+def test():
+    test_data = []
+    x_test = []
+    y_test = []
+    acc1 = []
+    with open("test1.csv", encoding='utf-8-sig') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        data = [dota for dota in rows]
+        for x in range(len(data)):
+            ins = []
+            ins_y = []
+            for y in range(4):
+                ins.append(float(data[x][y]))
+
+            c = data[x][4]
+            x_test.append(ins)
+            y_test.append(int(c))
+    model = bagging_algo()
+    X = x_test
+    y = y_test
+    X = normalise_data(X)
+    one_column = np.ones((len(X), 1))
+    # print("The normalised input data is", X)
+    X = np.concatenate((one_column, X), axis=1)
+    all_predictions = []
+    for m in model:
+        weight = m[2]
+        predict = predict_val(weight, X)
+        #print("predict is ",predict)
+        all_predictions.append(predict)
+        n = m[3]
+        #print("The actual output for the given input data is where '1' represents male and '0' representd female", y)
+        #print("The predicted output for the given data is", predict)
+        #print("len of y", len(y))
+
+        y_count = np.sum(y == predict)
+
+        Accuracy = (y_count / len(y)) * 100
+        #print("The calculated regression coefficients are", weight)
+        #print("total no of iterations taken", n)
+        #print("The count of the labels predicted", y_count)
+        #print("Accuracy is", (y_count / len(y)) * 100)
+        # print("y out",y)
+                # fit(x,y,1)
+        acc1.append(Accuracy)
+    #print("length of model",len(model))
+    print("Accuracy is",np.array(acc1))
+    print("Avg of accuracy",np.average(np.array(acc1)))
+    #print("all_predictions",all_predictions)
+    temp = []
+    avg_prediction = []
+
+    '''
+
+    for prediction in all_predictions:
+        for i in prediction:
+            temp.append(prediction[i])
+        val = np.argmax(np.bincount(temp))
+        
+    print("value is ",val)
+    '''
+    myshape = str(np.array(all_predictions).shape)
+    part1, part2 = myshape.split(', ')
+    part1 = part1[1:]  # now is '50'
+    part2 = part2[:-1]
+    #print("part1",part1)
+    #print("part2",part2)
+    val = []
+    for j in range(int(part2)):
+
+        ins = []
+        for i in range(int(part1)):
+
+            ins.append(all_predictions[i][j])
+        #print("ins",ins)
+        val.append(np.argmax(np.bincount(ins)))
+        temp.append(ins)
+
+    avg_prediction.append(val)
+    '''
+    print("value is ",avg_prediction[0])
+    print("actual value is ",y)
+    '''
+    y_count = 0
+    for i in range(len(y)):
+        if(y[i] == avg_prediction[0][i]):
+            y_count += 1
+
+
+    #y_count = np.sum(y == avg_prediction[0])
+    #print("y_count",y_count)
+
+    Accuracy = (y_count / len(y)) * 100
+
+
+    print("The accuracy after bagging is ",Accuracy)
+    return avg_prediction
+def bagging_iteration():
+    no_of_iterations = [10, 50, 100]
+    bagging_prediction_for_10 = []
+    bagging_prediction_for_50 = []
+    bagging_prediction_for_100 = []
+
+    for i in no_of_iterations:
+        #print("i is",i)
+        bagging_prediction = []
+        for j in range(i):
+            avg_prediction = test()
+            avg_prediction = avg_prediction[0]
+            #print("avg_prediction",len(avg_prediction))
+            bagging_prediction.append(avg_prediction)
+
+
+
+        print("bagging prediction",bagging_prediction)
+        if(i == 10):
+
+             avg_prediction_for_10 = calculate_avg_of_prediction(bagging_prediction)
+        if(i == 50):
+            temp = []
+            avg_prediction_for_50 = []
+            bagging_prediction_for_50 = bagging_prediction
+            myshape = str(np.array(bagging_prediction_for_50).shape)
+            part1, part2 = myshape.split(', ')
+            part1 = part1[1:]  # now is '50'
+            part2 = part2[:-1]
+            # print("part1",part1)
+            # print("part2",part2)
+            val = []
+            for j in range(int(part2)):
+
+                ins = []
+                for i in range(int(part1)):
+                    ins.append(bagging_prediction_for_50[i][j])
+                # print("ins",ins)
+                val.append(np.argmax(np.bincount(ins)))
+                temp.append(ins)
+
+            avg_prediction_for_50.append(val)
+        if(i ==100):
+            temp = []
+            avg_prediction_for_100 = calculate_avg_of_prediction(bagging_prediction)
+
+
+
+    print(" avg bagging_prediction_for_10",avg_prediction_for_10)
+    print("accuracy for 10 iteration", calculate_accuracy(y_test, avg_prediction_for_10))
+    print("error rate for 10 iteration",get_error_rate(y_test,avg_prediction_for_10) )
+    print("avg bagging_prediction_for_50", avg_prediction_for_50)
+    print("error rate for 50 iteration", get_error_rate(y_test, avg_prediction_for_50))
+    print("accuracy for 50 iterations",calculate_accuracy(y_test,avg_prediction_for_50))
+    print("avg_bagging_prediction_for_100", avg_prediction_for_100)
+    print("error rate for 100 iteration", get_error_rate(y_test, avg_prediction_for_100))
+    print("accuracy for 100 iterations", calculate_accuracy(y_test, avg_prediction_for_100))
+
+def calculate_avg_of_prediction(bagging_prediction):
+    temp = []
+    avg_prediction = []
+    bagging_prediction = bagging_prediction
+    myshape = str(np.array(bagging_prediction).shape)
+    part1, part2 = myshape.split(', ')
+    part1 = part1[1:]  # now is '50'
+    part2 = part2[:-1]
+    # print("part1",part1)
+    # print("part2",part2)
+
+    val = []
+    for j in range(int(part2)):
+
+        ins = []
+        for i in range(int(part1)):
+            ins.append(bagging_prediction[i][j])
+        # print("ins",ins)
+        val.append(np.argmax(np.bincount(ins)))
+        temp.append(ins)
+
+    avg_prediction.append(val)
+    return avg_prediction
+
+
+def calculate_accuracy(actual,predicted):
+
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[0][i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+def get_error_rate(actual,predicted):
+    error = 0
+    for i in range(len(actual)):
+        if actual[i] != predicted[0][i]:
+            error += 1
+    return error / float(len(actual)) * 100.0
+
+
+bagging_iteration()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ boosting_my.py
@@ -0,0 +1,349 @@
+import csv
+import math
+from random import seed
+from random import randrange
+from csv import reader
+from operator import itemgetter
+from random import randrange, seed
+import numpy as np
+from random import randrange, seed
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+def data_clean1():
+    x_train = []
+    y_train = []
+    test_data = []
+    x_test = []
+    y_test = []
+    with open("train1.csv",encoding='utf-8-sig') as csvfile:
+        rows = csv.reader(csvfile,delimiter=',')
+        data = [data for data in rows]
+        for x in range(len(data)):
+            ins = []
+            ins_y = []
+            for y in range(4):
+                ins.append(float(data[x][y]))
+
+            c = data[x][4]
+            y_train.append(float(c))
+
+            x_train.append(ins)
+
+
+    #print(np.array(x_train))
+
+    x = np.array(x_train)
+    #print("x  in data clean", x)
+    y = np.array(y_train)
+    #print("x in data clean,", x)
+    #print("y in data clean", y)
+        # x_train =
+        # y_train = y
+
+    with open("test1.csv", encoding='utf-8-sig') as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        data = [dota for dota in rows]
+        for a in range(len(data)):
+            ins = []
+            ins_y = []
+            for b in range(4):
+                ins.append(float(data[a][b]))
+
+            c = data[a][4]
+            x_test.append(ins)
+            y_test.append(int(c))
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+
+
+    return x, y,x_test,y_test
+
+
+
+def normalise_data(x):
+    " we can normalise by subtracting the maximum value by the minimum value and  then divide by the standard deviation of x or divide by the range"
+
+    max_x = np.max(x, axis=0)
+    min_x = np.min(x, axis=0)
+    normalised_x = 1 - (max_x - x) / (max_x - min_x)
+    # print(normalised_x)
+    return normalised_x
+
+def calculate_hypothesis_fn(x, weight):
+    #print("calculate sigmoid x", x)
+    #print("calculate sigmoid weight", weight)
+    h = 1.0 / (1 + np.exp(-np.dot(x, weight.T)))
+    return h
+
+def dot_product(a, b):
+    return sum(x * y for x, y in zip(a, b))
+
+def calculate_mean(self,m):
+    return (sum(x for x in m)) / len(m)
+
+def calculate_gradient(weight, x, y):
+        # X = X - y.reshape(X.shape[0] -1)
+        # print("y in gradient descent",y)
+        # print("weight in gradient descent",weight)
+        # print("x in gradient descent",x)
+    h = calculate_hypothesis_fn(x, weight)
+        # print("h in gradient decent",h)
+    a = h - y.reshape(x.shape[0], -1)
+    b = np.dot(a.T, x)
+        # print("grad in gradient",final_calc)
+    return b
+
+def calculate_cost(x, weight, y):
+    # calculating the hypothesis function
+    h = calculate_hypothesis_fn(x, weight)
+    # print("y before squeeze",y)
+    y = np.squeeze(y)
+
+    h = np.mean((-(y * np.log(h)) - ((1 - y)) * np.log(1 - h)))
+
+    return h
+
+
+def grad_descent(x, y, weight, lr, gradient):
+    cost = calculate_cost(x, weight, y)
+    # print(cost)
+    num_iteration, diff = 1, 1
+    while (diff > gradient):
+        # iteration += 1
+        prev_cost = cost
+        # finding the weights and updating it after every itertation. The loop goes on unti the condition is met
+        weight = weight - (lr * calculate_gradient(weight, x, y))
+        cost = calculate_cost(x, weight, y)
+        # print("cost in grad",cost)
+
+        diff = prev_cost - cost
+        # print("diff in grad", diff)
+        num_iteration = num_iteration + 1
+        # print("iteration no is",iteration)
+
+        # h = calculate_sigmoid(x, weight)
+
+
+    return weight, num_iteration
+
+def predict_class(weight, x):
+    prob = calculate_hypothesis_fn(x, weight)
+    prediction = np.where(prob >= .5, 1, 0)
+    return np.squeeze(prediction)
+
+def logistic_regression(x_train,y_train):
+    X, y = x_train,y_train
+    #X = normalise_data(X)
+    #one_column = np.ones((len(X), 1))
+    # print("The normalised input data is", X)
+    #X = np.concatenate((one_column, X), axis=1)
+    weight = np.matrix(np.zeros(5))
+    # print("xin main", X)
+    # print("y in main", y)
+    # print("beta in main", weight)
+    weight, n = grad_descent(X, y, weight, 0.01, 0.005)
+    predict = predict_class(weight, X)
+    # print(len(predict))
+    # print("predict",predict)
+    # print("the computed theta value is", weight)
+    # print("difference between y and predicted y")
+    # print("The acutal output for the given input data is where '1' represents male and '0' representd female", y)
+    # print("The predicted output for the given data is", predict)
+    # print("len of y",len(y))
+
+    y_count = np.sum(y == predict)
+
+    Accuracy = (y_count / len(y)) * 100
+    # print("The calculated regression coefficients are", weight)
+    # print("total no of iterations taken", n)
+    # print("The count of the labels predicted", y_count)
+    # print("Accuracy is",(y_count/len(y))*100)
+    # print("y out",y)
+    # fit(x,y,1)
+    # acc.append(Accuracy)
+    # plot_reg(X, y, weight)
+    return X, y, weight, n
+
+
+
+
+class Adaboost(object):
+
+    def __init__(self, X_train, y_train):
+        self.X_train = X_train
+
+        self.N = self.X_train.shape[0]
+
+        self.t_epsilon = []
+
+        self.hist = {}
+        self.y_train = y_train
+        self.weights = np.ones(self.N) / self.N
+        self.alphas = []
+        self.clfs = []
+        # self.num_estimators = T
+
+    def boosting_algo(self):
+        #         for t in range(self.num_estimators):
+        for t in range(100):
+            #print("t value is ",t)
+            output = np.random.choice(self.N, self.N, p=self.weights)
+
+            for t in output:
+                self.hist[t] = self.hist.get(t, 0) + 1
+
+            #             print output
+            #print("output is",output)
+            Xtrain_bst = self.X_train[output]
+            #print("Xtrain_bst is",Xtrain_bst)
+
+            ytrain_bst = self.y_train[output]
+
+            X,y,weight,n = logistic_regression(Xtrain_bst, ytrain_bst)
+            #             print ls.weights
+            #print("comes here")
+            #print("x b",X)
+            #self.X_train = X
+            Y_pred = predict_class(weight,self.X_train)
+
+
+
+            error_rate = np.sum((Y_pred != self.y_train) * self.weights)
+
+            if error_rate > 0.5:
+                weight = -weight
+                Y_pred = predict_class(weight,self.X_train)
+
+            #             print error_rate
+            self.t_epsilon.append(error_rate)
+
+
+            self.clfs.append(weight)
+            alpha_t = 0.5 * np.log((1 - error_rate) / error_rate)
+            self.alphas.append(alpha_t)
+
+
+            #             print alpha_t
+            self.weights = self.weights * np.exp(-alpha_t * Y_pred * self.y_train)
+            self.weights = self.weights / np.sum(self.weights)
+
+
+X_train,y_train,X_test,y_test = data_clean1()
+
+def x_append(data):
+    ones = np.ones((data.shape[0],1))
+    data = np.hstack((ones, data))
+    return data
+
+
+def iteration_25():
+    for t in tqdm(range(1, 26)):
+        #print("the value of t is ", t)
+        tot_train = np.zeros(X_train.shape[0])
+        tot_test = np.zeros(X_test.shape[0])
+        for i in range(t):
+            alpha = boost.alphas[i]
+            classifier = boost.clfs[i]
+            #             print X_train.shape
+            #             print classifier.weights.shape
+            #             print np.dot(X_train, classifier.weights).shape
+            #print("classifier", classifier[0])
+            tot_train += (alpha * predict_class(classifier[0], X_train))
+            tot_test += (alpha * predict_class(classifier[0], X_test))
+        s_train_pred = np.sign(tot_train)
+        s_test_pred = np.sign(tot_test)
+
+        training_error.append(np.sum(s_train_pred != y_train) / y_train.shape[0])
+        testing_error.append(np.sum(s_test_pred != y_test) / y_test.shape[0])
+    print("training_error for 25 iteration is ", training_error)
+    print("testing_error for 25  iteration is ", testing_error)
+
+    plt.figure()
+    #plotting the training error and testing error for values 1 to 25
+    plt.plot(range(1, 26), training_error, label="Training error")
+    plt.plot(range(1, 26), testing_error, label="Testing error")
+    plt.title("Training and Testing error of boost(t) for t = 1, 2, ... 25")
+    plt.legend()
+    plt.show()
+
+    plt.close()
+
+
+X_train = x_append(X_train)
+X_test = x_append(X_test)
+
+training_error = []
+testing_error = []
+
+boost = Adaboost(X_train, y_train)
+#print("x train in boosting",X_train)
+boost.boosting_algo()
+iteration_25()
+training_error1 = []
+testing_error1 = []
+for t in tqdm(range(1, 51)):
+    #print("the value of t is ",t)
+    tot_train = np.zeros(X_train.shape[0])
+    tot_test = np.zeros(X_test.shape[0])
+    for i in range(t):
+        alpha = boost.alphas[i]
+        classifier = boost.clfs[i]
+        #             print X_train.shape
+        #             print classifier.weights.shape
+        #             print np.dot(X_train, classifier.weights).shape
+        #print("classifier",classifier[0])
+        tot_train += (alpha * predict_class(classifier[0],X_train))
+        tot_test += (alpha * predict_class(classifier[0],X_test))
+    s_train_pred = np.sign(tot_train)
+    s_test_pred = np.sign(tot_test)
+
+    training_error1.append(np.sum(s_train_pred != y_train) / y_train.shape[0])
+    testing_error1.append(np.sum(s_test_pred != y_test) / y_test.shape[0])
+print("training_error for 50 iteration is ",training_error1)
+print("testing_error for 50 iteration is ",testing_error1)
+#plotting the training error and testing error for values 1 to 50
+plt.figure()
+plt.plot(range(1, 51), training_error1, label="Training error")
+plt.plot(range(1, 51), testing_error1, label="Testing error")
+plt.title("Training and testing error of boost(t) for values t = 1, 2, ... 50")
+plt.legend()
+plt.show()
+
+plt.close()
+testing_error2 = []
+training_error2 = []
+
+for t in tqdm(range(1, 11)):
+   # print("the value of t is ",t)
+
+    tot_test = np.zeros(X_test.shape[0])
+    tot_train = np.zeros(X_train.shape[0])
+
+    for i in range(t):
+        alpha = boost.alphas[i]
+        classifier = boost.clfs[i]
+        #             print X_train.shape
+        #             print classifier.weights.shape
+        #             print np.dot(X_train, classifier.weights).shape
+        #print("classifier",classifier[0])
+        tot_train += (alpha * predict_class(classifier[0],X_train))
+        tot_test += (alpha * predict_class(classifier[0],X_test))
+    s_train_pred = np.sign(tot_train)
+    s_test_pred = np.sign(tot_test)
+    #appending to training error
+    testing_error2.append(np.sum(s_test_pred != y_test) / y_test.shape[0])
+    #appending the result to testing error
+    training_error2.append(np.sum(s_train_pred != y_train) / y_train.shape[0])
+
+print("training_error for 10 iteration is ",training_error2)
+print("testing_error for 10 iteration is ",testing_error2)
+
+plt.figure()
+plt.plot(range(1, 11), training_error2, label="Training error")
+plt.plot(range(1, 11), testing_error2, label="Testing error")
+plt.title("Training and Testing error of boost for values  t = 1, 2, ... 10")
+plt.legend()
+plt.show()
+
+plt.close()
